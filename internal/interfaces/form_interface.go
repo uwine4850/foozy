3d76322238f7f8681a325db59c986a06a479cdf5
uwine4850 <uwine4850@gmail.com>
2023-10-01 13:08:04 +0300

    Made methods for processing the form.

---
+++ internal/interfaces/form_interface.go
@@ -0,0 +1,14 @@
+package interfaces
+
+import (
+	"mime/multipart"
+	"net/url"
+)
+
+type IForm interface {
+	Parse() error
+	GetMultipartForm() *multipart.Form
+	GetApplicationForm() url.Values
+	Value(key string) string
+	File(key string) (multipart.File, *multipart.FileHeader, error)
+}
---
+++ internal/router/form/form.go
@@ -0,0 +1,57 @@
+package form
+
+import (
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Form struct {
+	multipartForm      *multipart.Form
+	applicationForm    url.Values
+	multipartMaxMemory int64
+	request            *http.Request
+}
+
+func NewForm(request *http.Request) *Form {
+	return &Form{multipartMaxMemory: 32 << 20, request: request}
+}
+
+// Parse parsing a form method. After that you can access fields by name and the form in general.
+func (f *Form) Parse() error {
+	enctype := strings.Split(f.request.Header.Get("Content-Type"), ";")[0]
+	switch enctype {
+	case "application/x-www-form-urlencoded":
+		err := f.request.ParseForm()
+		if err != nil {
+			return err
+		}
+		f.applicationForm = f.request.PostForm
+	case "multipart/form-data":
+		err := f.request.ParseMultipartForm(f.multipartMaxMemory)
+		if err != nil {
+			return err
+		}
+		f.multipartForm = f.request.MultipartForm
+	}
+	return nil
+}
+
+func (f *Form) GetMultipartForm() *multipart.Form {
+	return f.multipartForm
+}
+
+func (f *Form) GetApplicationForm() url.Values {
+	return f.applicationForm
+}
+
+// Value getting a simple value of the form.
+func (f *Form) Value(key string) string {
+	return f.request.PostFormValue(key)
+}
+
+// File retrieving a file from a form. Multipart/form-data only.
+func (f *Form) File(key string) (multipart.File, *multipart.FileHeader, error) {
+	return f.request.FormFile(key)
+}
